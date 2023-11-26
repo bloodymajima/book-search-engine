d46d89ad6bf5be3caf2f2bf42842b017e959a7c4
bloodymajima <mdeavers18@gmail.com>
2023-11-26 14:41:23 -0500

    Added apolloServer to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,21 @@ import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <>
+        <Navbar />
+        <Outlet />
+      </>
+    </ApolloProvider>
   );
 }
 
